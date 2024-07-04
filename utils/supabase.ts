948742a837d054b4296c194dd3b47c26419e3933
BonhaengLee <coalee22@gmail.com>
2024-07-04 21:33:21 +0900

    :wrench: Environment variables in Expo

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,13 @@
-import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("supabaseUrl and supabaseAnonKey are required.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {

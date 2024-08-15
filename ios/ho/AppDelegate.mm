f1fdfa4a9013893ad39984c8ad7911bb26fbb94c
BonhaengLee <coalee22@gmail.com>
2024-08-15 15:01:16 +0900

    :fire: kakao login 관련 세팅 제거

--- ios/ho/AppDelegate.mm
+++ ios/ho/AppDelegate.mm
@@ -2,17 +2,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
-#import <RNCKakaoUser/RNCKakaoUserUtil.h>
 
 @implementation AppDelegate
 
 // Linking API
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-
-  // 카카오톡에서 전달된 URL scheme면 이 앱에서 핸들링하는 로직입니다.
-  if([RNCKakaoUserUtil isKakaoTalkLoginUrl:url]) {
-    return [RNCKakaoUserUtil handleOpenUrl:url];
-  }
 
   return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options];
 }

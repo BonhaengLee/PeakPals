67056cc3e98704d44305095488dc6958a92cd00e
BonhaengLee <coalee22@gmail.com>
2024-09-26 16:28:26 +0900

    :lipstick: 바텀시트 UI

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,36 +1,188 @@
-import React, {
-  useEffect,
-  useState,
-  useRef,
-  useMemo,
-  useCallback,
-} from "react";
+import BottomSheet, {
+  BottomSheetBackgroundProps,
+  BottomSheetScrollView,
+} from "@gorhom/bottom-sheet";
 import {
-  Alert,
-  StyleSheet,
-  View,
-  Text,
-  ActivityIndicator,
-  Linking,
-  Pressable,
-  SafeAreaView,
-  Image,
-  TouchableWithoutFeedback,
-} from "react-native";
-import {
+  NaverMapMarkerOverlay,
   NaverMapView,
   NaverMapViewRef,
-  NaverMapMarkerOverlay,
 } from "@mj-studio/react-native-naver-map";
 import * as Location from "expo-location";
-import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  Linking,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-import { ClimbingCenter } from "../types";
 import { supabase } from "../lib/supabaseClient";
+import { ClimbingCenter, SimplifiedCenter } from "../types";
+import { colors } from "../styles/colors";
 
 // MainTabs 높이 + 40px을 계산
 const MAIN_TABS_HEIGHT = 92; // MainTabs의 높이
 const MIN_SHEET_HEIGHT = MAIN_TABS_HEIGHT + 40; // 항상 최소한 이 높이까지만 내려가게 함
+
+interface SavedCenter extends SimplifiedCenter {
+  isSaved: boolean;
+  // 각 유저별로 다른 값을 가져야 함
+  visitCount: number;
+}
+
+// FIXME: mock data
+const savedCenters: SavedCenter[] = [
+  {
+    id: 1,
+    name: "클라이밍 센터 1",
+    address: "서울특별시 강남구 테헤란로 123",
+    visitCount: 12,
+    isSaved: true,
+  },
+  {
+    id: 2,
+    name: "클라이밍 센터 2",
+    address: "서울특별시 강남구 테헤란로 123",
+    visitCount: 12,
+    isSaved: true,
+  },
+  {
+    id: 3,
+    name: "클라이밍 센터 3",
+    address: "서울특별시 강남구 테헤란로 123",
+    visitCount: 12,
+    isSaved: false,
+  },
+  {
+    id: 4,
+    name: "클라이밍 센터 4",
+    address: "서울특별시 강남구 테헤란로 123",
+    visitCount: 122,
+    isSaved: false,
+  },
+  {
+    id: 5,
+    name: "클라이밍 센터 5",
+    address: "서울특별시 강남구 테헤란로 123",
+    visitCount: 132,
+    isSaved: false,
+  },
+  {
+    id: 6,
+    name: "클라이밍 센터 6",
+    address: "서울특별시 강남구 테헤란로 123",
+    visitCount: 412,
+    isSaved: false,
+  },
+];
+
+// mock nearby centers
+const nearbyCenters: SimplifiedCenter[] = [
+  {
+    id: 4,
+    name: "내 주변 센터 1",
+    address: "서울특별시 강남구 역삼동 456",
+  },
+  {
+    id: 5,
+    name: "내 주변 센터 2",
+    address: "서울특별시 강남구 논현동 789",
+  },
+  {
+    id: 6,
+    name: "내 주변 센터 3",
+    address: "서울특별시 강남구 삼성동 123",
+  },
+];
+
+const renderSavedCenters = (centers: SavedCenter[]) => {
+  return centers.map((center, index) => (
+    <View key={index} style={styles.centerContainer}>
+      <View>
+        <Text style={styles.centerName}>{center.name}</Text>
+        <Text style={styles.centerAddress}>{center.address}</Text>
+        <View style={styles.visitCountContainer}>
+          <Text style={styles.visitCountText}>{center.visitCount}번 방문</Text>
+        </View>
+      </View>
+      <Image
+        style={styles.starIcon}
+        source={require("../assets/images/maps/filledStar.png")}
+      />
+    </View>
+  ));
+};
+
+const renderNearbyCenters = (centers: SimplifiedCenter[]) => {
+  return centers.map((center, index) => (
+    <View key={index} style={styles.centerContainer}>
+      <View>
+        <Text style={styles.centerName}>{center.name}</Text>
+        <Text style={styles.centerAddress}>{center.address}</Text>
+        {/* FIXME: Uncomment when ready to implement
+        <Text style={styles.openStatus}>영업중 XX</Text>
+        */}
+      </View>
+    </View>
+  ));
+};
+
+const renderSegmentedControl = ({
+  activeTab,
+  setActiveTab,
+}: {
+  activeTab: string;
+  setActiveTab: Function;
+}) => {
+  return (
+    <View style={styles.segmentedControl}>
+      <Pressable
+        style={[
+          styles.segmentButton,
+          styles.segmentButtonLeft,
+          activeTab === "saved" && styles.activeSegment,
+        ]}
+        onPress={() => setActiveTab("saved")}
+      >
+        <Text
+          style={[
+            styles.segmentButtonText,
+            activeTab === "saved" && styles.activeSegmentText,
+          ]}
+        >
+          저장된 센터
+        </Text>
+      </Pressable>
+      <Pressable
+        style={[
+          styles.segmentButton,
+          styles.segmentButtonRight,
+          activeTab === "nearby" && styles.activeSegment,
+        ]}
+        onPress={() => setActiveTab("nearby")}
+      >
+        <Text
+          style={[
+            styles.segmentButtonText,
+            activeTab === "nearby" && styles.activeSegmentText,
+          ]}
+        >
+          내 주변 센터
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const checkIfLocationServicesEnabled = async () => {
   const isEnabled = await Location.hasServicesEnabledAsync();
@@ -154,19 +306,14 @@ export default function HomeScreen() {
     setSheetIndex(index);
   }, []);
 
-  const renderContent = () => (
-    <BottomSheetScrollView contentContainerStyle={styles.bottomSheetContent}>
-      <Text style={styles.modalText}>
-        바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트
-        내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트
-        내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트
-        내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트
-        내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트
-        내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트 내용바텀 시트
-        내용바텀 시트 내용바텀 시트 내용바텀 시트 내용
-      </Text>
-    </BottomSheetScrollView>
-  );
+  const [activeTab, setActiveTab] = useState("saved");
+  const renderCenters = () => {
+    if (activeTab === "saved") {
+      return renderSavedCenters(savedCenters);
+    } else if (activeTab === "nearby") {
+      return renderNearbyCenters(nearbyCenters);
+    }
+  };
 
   const [centers, setCenters] = useState<ClimbingCenter[]>([]); // Supabase에서 가져온 센터 데이터
   useEffect(() => {
@@ -195,6 +342,29 @@ export default function HomeScreen() {
       </View>
     ),
     []
+  );
+
+  const renderContent = useCallback(
+    () => (
+      <BottomSheetScrollView
+        contentContainerStyle={styles.bottomSheetScrollViewContent}
+      >
+        {renderSegmentedControl({ activeTab, setActiveTab })}
+        {renderCenters()}
+      </BottomSheetScrollView>
+    ),
+    [activeTab]
+  );
+
+  const CustomBackground = ({ style }: BottomSheetBackgroundProps) => (
+    <View
+      style={[
+        style,
+        {
+          backgroundColor: colors.ui04,
+        },
+      ]}
+    />
   );
 
   if (!location) {
@@ -258,8 +428,9 @@ export default function HomeScreen() {
           enableOverDrag={false}
           enableContentPanningGesture={true}
           onChange={handleSheetChanges}
+          backgroundComponent={CustomBackground}
         >
-          <View style={styles.bottomSheetContent}>{renderContent()}</View>
+          {renderContent()}
         </BottomSheet>
       </View>
 
@@ -294,10 +465,6 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
   },
-  bottomSheetContent: {
-    padding: 20,
-    height: "100%",
-  },
   bottomSheetContainer: {
     position: "absolute",
     left: 0,
@@ -305,26 +472,39 @@ const styles = StyleSheet.create({
     bottom: 0,
     height: "100%",
     zIndex: 2,
+    padding: 16,
+    // backgroundColor: colors.ui04,
   },
   bottomSheetHandle: {
     height: 24,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    backgroundColor: colors.ui04,
   },
   handleBar: {
     width: 40,
     height: 4,
-    backgroundColor: "#00000040",
+    backgroundColor: colors.bottomSheetHandleBlack,
     borderRadius: 2,
+  },
+  bottomSheetScrollViewContent: {
+    padding: 20,
+    backgroundColor: colors.ui04,
+    flexGrow: 1,
+  },
+  bottomSheetContent: {
+    padding: 20,
+    height: "100%",
+    backgroundColor: colors.ui03,
   },
   modalText: {
     fontSize: 16,
     marginBottom: 20,
     color: "red",
     fontSize: 46,
+    height: "100%",
   },
   locationButton: {
     position: "absolute",
@@ -355,5 +535,76 @@ const styles = StyleSheet.create({
   loadingText: {
     fontSize: 16,
     marginBottom: 20,
+  },
+  centerContainer: {
+    backgroundColor: colors.ui03,
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  centerName: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  centerAddress: {
+    color: "rgba(255, 255, 255, 0.5)",
+    fontSize: 14,
+  },
+  visitCountContainer: {
+    marginTop: 8,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderColor: "rgba(255, 255, 255, 0.5)",
+    borderWidth: 1,
+    borderRadius: 16,
+  },
+  visitCountText: {
+    color: colors.textWhite,
+    fontSize: 14,
+  },
+  starIcon: {
+    width: 32,
+    height: 32,
+  },
+  openStatus: {
+    color: "#d1d5db",
+    fontSize: 14,
+    marginTop: 8,
+  },
+  segmentedControl: {
+    flexDirection: "row",
+    marginBottom: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  segmentButton: {
+    flex: 1,
+    paddingVertical: 8,
+    alignItems: "center",
+    backgroundColor: colors.ui03,
+    color: colors.coreSecondary,
+  },
+  segmentButtonLeft: {
+    borderTopLeftRadius: 8,
+    borderBottomLeftRadius: 8,
+  },
+  segmentButtonRight: {
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+  segmentButtonText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: colors.text02,
+  },
+  activeSegment: {
+    backgroundColor: colors.primary,
+  },
+  activeSegmentText: {
+    color: colors.textBlack,
   },
 });
